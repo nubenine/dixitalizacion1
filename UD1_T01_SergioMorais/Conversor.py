a3cbd3959fea09f7dab86e972f9966583689d8d9
sergio10projmor@gmail.com <sergio10projmor@gmail.com>
2026-09-15 11:39:51 +0200

    Conversor

---
+++ UD1_T01_SergioMorais/Conversor.py
@@ -0,0 +1,8 @@
+celsius = int(input("Dame la temperatura en grados celsius, muchacho "))
+faren = (celsius*1.8) + 32
+kelvin = celsius + 273,15
+
+
+print(f"En Celsius: {celsius}º")
+print(f"Em faren: {faren}º")
+print(f"En Kelvin: {kelvin}º")
